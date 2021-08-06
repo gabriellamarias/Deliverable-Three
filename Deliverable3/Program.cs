967f607e5c2d5ac64dc2b564927152ae2c395f9f
gabi <gabriella.marias@gmail.com>
2021-08-05 20:21:43 -0400

    Second commit for deliverable three. Added a failsafe for non-integer user input and more seamless looping

--- Deliverable3/Program.cs
+++ Deliverable3/Program.cs
@@ -8,6 +8,7 @@ namespace Deliverable3
         {
             string userInputContinue = "y";
             bool userContinue = true;
+            bool userTryAgain = true;
 
             string userNumberInput = "";
             int userNumber = 0;
@@ -21,65 +22,90 @@ namespace Deliverable3
 
             while (userContinue == true)
             {
+                userTryAgain = true;
+
                 Console.Write(userName + ", please enter a whole number between 1 and 100: ");
-
                 userNumberInput = Console.ReadLine();
-                userNumber = Int16.Parse(userNumberInput);
+                if (int.TryParse(userNumberInput, out userNumber))
+                {
+                }
 
-                if (userNumber <= 0)
+                else
+                {
+                    Console.WriteLine("Now, now, an integer, if you please.");
+                    userContinue = true;
+                }
+
+
+                if (userNumber < 0)
                 {
                     Console.WriteLine("Now, now, " + userName + " you know the rules! Please pick a number higher than 0");
+                    userContinue = true;
                 }
 
                 else if (userNumber > 100)
                 {
                     Console.WriteLine(userName + "! Are you trying to break me?! I'm a very fragile machine, that number is far too high!");
+                    userContinue = true;
                 }
 
-                else if (userNumber % 2 != 0 && userNumber > 60)
+                else if (userNumber % 2 != 0 & userNumber > 60)
                 {
                     Console.WriteLine(userNumber + "...very peculiar...I mean odd!");
+                    userContinue = false;
                 }
 
-                else if (userNumber % 2 == 0 && userNumber >= 2 && userNumber <= 25)
+                else if (userNumber % 2 == 0 & userNumber >= 2 && userNumber <= 25)
                 {
                     Console.WriteLine("The number is even, and less than 25.");
+                    userContinue = false;
                 }
 
-                else if (userNumber % 2 == 0 && userNumber >= 26 && userNumber <= 60)
+                else if (userNumber % 2 == 0 & userNumber >= 26 && userNumber <= 60)
                 {
                     Console.WriteLine("...even...even Steven! Isn't that your name? Oh, my apologies, " + userName);
+                    userContinue = false;
                 }
 
-                else if (userNumber % 2 == 0 && userNumber > 60)
+                else if (userNumber % 2 == 0 & userNumber > 60)
                 {
                     Console.WriteLine(userNumber + " is an even number.");
+                    userContinue = false;
                 }
 
                 else if (userNumber % 2 != 0)
                 {
                     Console.WriteLine(userNumber + "...what an odd number to input...very odd indeed...");
-                }
-
-
-                Console.Write("Do you wish to continue? (y/n): ");
-                userInputContinue = Console.ReadLine().ToLower();
-
-                if (userInputContinue == "y")
-                {
-                    userContinue = true;
-
-                }
-                else if (userInputContinue == "n")
-                {
-                    Console.WriteLine("Thank you for spending time with this rickity old machine! Safe travels, " + userName + ".");
                     userContinue = false;
-
-                    System.Environment.Exit(0);
-
                 }
 
 
+                while (userTryAgain == true & userContinue == false)
+
+                {
+                    Console.Write("Do you wish to continue? (y/n): ");
+                    userInputContinue = Console.ReadLine().ToLower();
+
+                    if (userInputContinue == "y")
+                    {
+                        userContinue = true;
+                        userTryAgain = false;
+                    }
+                    else if (userInputContinue == "n")
+                    {
+                        Console.WriteLine("Thank you for spending time with this rickity old machine! Safe travels, " + userName + ".");
+                        userContinue = false;
+                        userTryAgain = false;
+
+                        System.Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("I didn't understand that, please try again.");
+                        userTryAgain = true;
+                    }
+
+                }
             }
         }
     }
